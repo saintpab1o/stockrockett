03b75fa14440f1b1744a5b02c97af988e9c018f1
saintpablo <pau1magic@aim.com>
2020-06-28 22:31:31 -0400

    charting negative values

--- src/index.js
+++ src/index.js
@@ -78,39 +78,98 @@ axios.get('https://sandbox.iexapis.com/stable/stock/SQ/chart/1m?&filter=changePe
 
     
         // console.log(sqArray)
-        // console.log(mondayMonthlyPercent, 'sq monday')
-        // console.log(tuesdayMonthlyPercent, 'sq tuesday') 
-        // console.log(wednesdayMonthlyPercent, 'sq wednesday') 
-        // console.log(thursdayMonthlyPercent, 'sq thursday') 
-        // console.log(fridayMonthlyPercent, 'sq friday')  
+        console.log(mondayMonthlyPercent, 'sq monday')
+        console.log(tuesdayMonthlyPercent, 'sq tuesday') 
+        console.log(wednesdayMonthlyPercent, 'sq wednesday') 
+        console.log(thursdayMonthlyPercent, 'sq thursday') 
+        console.log(fridayMonthlyPercent, 'sq friday')  
 
-        // let sqArray = [mondayMonthlyPercent, tuesdayMonthlyPercent, wednesdayMonthlyPercent, thursdayMonthlyPercent, fridayMonthlyPercent]
-        let sqArray = [50, 20, 5, 30, 10]
-
-        var svgWidth = 500, svgHeight = 350, barPadding = 10;
-        var barWidth = (svgWidth / sqArray.length - 5 );
+         var data = [mondayMonthlyPercent, tuesdayMonthlyPercent, wednesdayMonthlyPercent, thursdayMonthlyPercent, fridayMonthlyPercent]
+        // let sqArray = [50, 20, 5, 30, 10]
 
 
-        var svg = d3.select('svg')
-            .attr("width", svgWidth)
-            .attr("height", svgHeight);
+        // var data = [100, -100, -150, 55, 150, 120, 450, 980, 1200];
 
-        var barChart = svg.selectAll("rect")
-            .data(sqArray)
+        var leftMargin = 50;  // Space to the left of first bar; accomodates y-axis labels
+        var rightMargin = 10; // Space to the right of last bar
+        var margin = { left: leftMargin, right: rightMargin, top: 10, bottom: 10 };
+        var barWidth = 30;  // Width of the bars
+        var chartHeight = 450;  // Height of chart, from x-axis (ie. y=0)
+        var chartWidth = margin.left + data.length * barWidth + margin.right;
+
+        /* This scale produces negative output for negatve input */
+        var yScale = d3.scaleLinear()
+            .domain([0, d3.max(data)])
+            .range([0, chartHeight]);
+
+        /*
+         * We need a different scale for drawing the y-axis. It needs
+         * a reversed range, and a larger domain to accomodate negaive values.
+         */
+        var yAxisScale = d3.scaleLinear()
+            .domain([d3.min(data), d3.max(data)])
+            .range([chartHeight - yScale(d3.min(data)), 0]);
+
+        var svg = d3.select('svg');
+        svg
+            .attr('height', chartHeight + 100)
+            .attr('width', chartWidth)
+            .style('border', '1px solid');
+
+        svg
+            .selectAll("rect")
+            .data(data)
             .enter()
             .append("rect")
-            .attr("y", function (d) {
-                return svgHeight - d * 2
+            .attr("x", function (d, i) { return margin.left + i * barWidth; })
+            .attr("y", function (d, i) { return chartHeight - Math.max(0, yScale(d)); })
+            .attr("height", function (d) { return Math.abs(yScale(d)); })
+            .attr("width", barWidth)
+            .style("fill", "grey")
+            .style("stroke", "black")
+            .style("stroke-width", "1px")
+            .style("opacity", function (d, i) { return 1 /*- (i * (1/data.length)); */ });
+
+        var yAxis = d3.axisLeft(yAxisScale);
+
+        svg.append('g')
+            .attr('transform', function (d) {
+                return 'translate(' + margin.left + ', 0)';
             })
-            .attr("height", function (d) {
-                return d * 2
-            })
-            .attr("width", barWidth - barPadding)
-            .attr("transform", function (d, i) {
-                var translate = [barWidth * i, 0];
-                return "translate(" + translate + ")";
+            .call(yAxis);
+
+    /*
+    var xScale = d3.scaleLinear()
+                    .domain(0, data.length * barWidth)
+                    .range(0, chartWidth);
+
+    var xAxis = d3.axisBottom();
+    */
+
+        // var svgWidth = 500, svgHeight = 350, barPadding = 10;
+        // var barWidth = (svgWidth / sqArray.length - 5 );
+
+
+        // var svg = d3.select('svg')
+        //     .attr("width", svgWidth)
+        //     .attr("height", svgHeight);
+
+        // var barChart = svg.selectAll("rect")
+        //     .data(sqArray)
+        //     .enter()
+        //     .append("rect")
+        //     .attr("y", function (d) {
+        //         return svgHeight - d * 2
+        //     })
+        //     .attr("height", function (d) {
+        //         return d * 2
+        //     })
+        //     .attr("width", barWidth - barPadding)
+        //     .attr("transform", function (d, i) {
+        //         var translate = [barWidth * i, 0];
+        //         return "translate(" + translate + ")";
     })
-});
+
 
 
 // // export const getAaplThirtyDayAvg = () => {
