e1ef5a538df78cddd2bc60b6086075432d3e86d6
saintpab1o <pau1magic@aim.com>
2022-02-26 00:19:09 -0500

    BOO

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const iex = new IEXCloudClient(fetch, {
     version: "stable"
 });
 
-axios.get('https://sandbox.iexapis.com/stable/stock/fb/chart/1m?token=Tsk_4fc15c0c3dab49d284a00d5c440fc609')
+axios.get('https://sandbox.iexapis.com/stable/stock/sq/chart/1m?token=Tsk_4fc15c0c3dab49d284a00d5c440fc609')
     .then(function (response) {
 
         const monday = [
