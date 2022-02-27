75e64d5d4e80e97255f899bf271bf34f70f9502c
saintpab1o <pau1magic@aim.com>
2022-02-27 03:24:07 -0500

    shot

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import * as d3 from 'd3'
 
 
  //WHEN PAGE LOADS
-console.log('hello')
+
 const iex = new IEXCloudClient(fetch, {
     sandbox: true,
     publishable: "Tpk_7191aab3200940d588ebad397e1e7e0d",
